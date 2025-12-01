87fa72c86de16848cf51dab68d22c4fc7471043c
ASADHASSAN <cybersecjunior00@gmail.com>
2025-12-01 17:45:48 +0000

    Solving ACM certificate issue

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -4,4 +4,5 @@ variable "project_name" { type = string }
 variable "bucket_name" { type = string }
 variable "domain_name" { type = string }
 variable "hosted_zone_id" { type = string }
+variable "acm_certificate_arn" { type = string }
 variable "tags" { type = map(string) }
