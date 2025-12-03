84f9d6dd820c9dd943e39e9bba01224319b17467
ASADHASSAN <cybersecjunior00@gmail.com>
2025-12-03 19:56:24 +0000

    last try

--- Terraform/modules/cloudfront/main.tf
+++ Terraform/modules/cloudfront/main.tf
@@ -44,6 +44,8 @@ ssl_support_method = "sni-only"
 tags = var.tags
 }
 
+
+
 output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.cdn.domain_name
 }
@@ -51,3 +53,4 @@ output "cloudfront_domain_name" {
 output "cloudfront_distribution_id" {
   value = aws_cloudfront_distribution.cdn.id
 }
+
--- Terraform/modules/s3/main.tf
+++ Terraform/modules/s3/main.tf
@@ -19,11 +19,11 @@ suffix = "index.html"
 }
 }
 
-resource "aws_s3_bucket_public_access_block" "public_access" {
+resource "aws_s3_bucket_public_access_block" "block" {
 bucket = aws_s3_bucket.site.id
-block_public_acls = false
-block_public_policy = false
-ignore_public_acls = false
+block_public_acls = true
+block_public_policy = true    
+ignore_public_acls = true
 restrict_public_buckets = false
 }
 
