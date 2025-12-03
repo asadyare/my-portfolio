2913315111f5a8001015e4f4914b43b1ac184abe
ASADHASSAN <cybersecjunior00@gmail.com>
2025-12-03 18:41:55 +0000

    Test 2

--- Terraform/modules/acm/main.tf
+++ Terraform/modules/acm/main.tf
@@ -2,15 +2,12 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
+      configuration_aliases = [ 
+        aws.us_east_1
+      ]
     }
   }
 }
-
-provider "aws" {
-  alias = "us_east_1"
-}
-
-
 
 
 locals {
--- Terraform/modules/cloudfront/main.tf
+++ Terraform/modules/cloudfront/main.tf
@@ -43,3 +43,11 @@ ssl_support_method = "sni-only"
 
 tags = var.tags
 }
+
+output "cloudfront_domain_name" {
+  value = aws_cloudfront_distribution.cdn.domain_name
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.cdn.id
+}
