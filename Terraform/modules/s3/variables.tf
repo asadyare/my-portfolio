805edcf17a8f6fbaefd6893b9c7104ecec598ee6
ASADHASSAN <cybersecjunior00@gmail.com>
2025-12-22 11:37:47 +0000

    Fixing Terraform Scan Checkov errors-04

--- Terraform/modules/s3/variables.tf
+++ Terraform/modules/s3/variables.tf
@@ -1,3 +1,11 @@
-variable "bucket_name" { type = string }
-variable "tags" { type = map(string) }
-variable "replica_bucket_arn" { type = string }
+variable "buckets" {
+  type = list(object({
+    name            = string
+    type            = string
+    replication_arn = string
+  }))
+}
+
+variable "tags" {
+  type = map(string)
+}
