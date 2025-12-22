1e8a0f52b6f4bce25e46a3731743bc9865194315
ASADHASSAN <cybersecjunior00@gmail.com>
2025-12-22 07:47:01 +0000

    Fixing Terraform modules scan error

--- Terraform/modules/cloudfront/main.tf
+++ Terraform/modules/cloudfront/main.tf
@@ -6,51 +6,111 @@ terraform {
   }
 }
 
-resource "aws_cloudfront_distribution" "cdn" {
-enabled = true
+resource "aws_cloudfront_response_headers_policy" "security" {
+  name = "security-headers"
 
-origin {
-domain_name = var.bucket_domain
-origin_id = "s3-origin"
-}
+  security_headers_config {
+    content_security_policy {
+      content_security_policy = "default-src https:"
+      override = true
+    }
 
-default_cache_behavior {
-target_origin_id = "s3-origin"
-viewer_protocol_policy = "redirect-to-https"
-allowed_methods = ["GET", "HEAD"]
-cached_methods = ["GET", "HEAD"]
+    strict_transport_security {
+      access_control_max_age_sec = 63072000
+      include_subdomains = true
+      preload = true
+      override = true
+    }
 
-forwarded_values {
-  query_string = false
-  cookies {
-    forward = "none"
+    xss_protection {
+      protection = true
+      mode_block = true
+      override = true
+    }
   }
 }
 
+resource "aws_wafv2_web_acl" "cf" {
+  name  = "cloudfront-waf"
+  scope = "CLOUDFRONT"
 
+  default_action {
+    allow {}
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name = "commonRules"
+      sampled_requests_enabled = true
+    }
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name = "cloudfrontWAF"
+    sampled_requests_enabled = true
+  }
 }
 
-restrictions {
-geo_restriction {
-restriction_type = "none"
+resource "aws_cloudfront_distribution" "cdn" {
+  enabled             = true
+  default_root_object = "index.html"
+  web_acl_id          = aws_wafv2_web_acl.cf.arn
+
+  origin {
+    domain_name = var.bucket_domain
+    origin_id   = "s3-origin"
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD"]
+    cached_methods         = ["GET", "HEAD"]
+
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.security.id
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["GB"]
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = var.certificate_arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+
+  logging_config {
+    bucket          = var.logs_bucket_domain
+    include_cookies = false
+    prefix          = "cloudfront/"
+  }
+
+  tags = var.tags
 }
-}
-
-viewer_certificate {
-acm_certificate_arn = var.certificate_arn
-ssl_support_method = "sni-only"
-}
-
-tags = var.tags
-}
-
-
-
-output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.cdn.domain_name
-}
-
-output "cloudfront_distribution_id" {
-  value = aws_cloudfront_distribution.cdn.id
-}
-
--- Terraform/modules/cloudfront/variables.tf
+++ Terraform/modules/cloudfront/variables.tf
@@ -2,3 +2,4 @@ variable "bucket_domain" { type = string }
 variable "certificate_arn" { type = string }
 variable "domain_name" { type = string }
 variable "tags" { type = map(string) }
+variable "logs_bucket_domain" { type = string }
