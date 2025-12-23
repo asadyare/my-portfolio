69059e41ac871bb5569f1531bd8902d9147e738e
ASADHASSAN <cybersecjunior00@gmail.com>
2025-12-23 10:14:40 +0000

    Fixing Terraform Scan Checkov errors-11

--- Terraform/modules/cloudfront/main.tf
+++ Terraform/modules/cloudfront/main.tf
@@ -61,11 +61,11 @@ signing_protocol = "sigv4"
 }
 
 resource "aws_cloudfront_distribution" "this" {
-# logging_config {
-# bucket = var.logging_bucket_domain_name
-# include_cookies = false
-# prefix = var.logging_prefix
-# }
+logging_config {
+bucket = var.logging_bucket_domain_name
+include_cookies = false
+prefix = var.logging_prefix
+}
 # web_acl_id = aws_wafv2_web_acl.this.arn
 enabled = true
 # is_ipv6_enabled = true
@@ -124,8 +124,8 @@ restriction_type = "whitelist"
 
 viewer_certificate {
 cloudfront_default_certificate = true
-# acm_certificate_arn = var.acm_certificate_arn
-# ssl_support_method = "sni-only"
-# minimum_protocol_version = "TLSv1.2_2021"
+acm_certificate_arn = var.acm_certificate_arn
+ssl_support_method = "sni-only"
+minimum_protocol_version = "TLSv1.2_2021"
 }
 }
