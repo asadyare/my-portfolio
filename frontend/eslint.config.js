9e45f5131046b49d6f5711f7685192934ad9755b
ASADHASSAN <cybersecjunior00@gmail.com>
2025-12-31 11:16:13 +0000

    fixing gitleaks

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -2,7 +2,7 @@ import babelParser from "@babel/eslint-parser";
 
 export default [
   {
-    files: ["frontend/src/**/*.{js,jsx}"],
+    files: ["frontend/src/**/*.{jsx,js}"],
     languageOptions: {
       parser: babelParser,
       parserOptions: {
