4a0a9c41322cb40cb252526947abf98e98abc35c
ASADHASSAN <cybersecjunior00@gmail.com>
2026-01-01 16:11:02 +0000

    fixing hosted zone dnssec

--- Terraform/modules/dns/outputs.tf
+++ Terraform/modules/dns/outputs.tf
@@ -1,4 +1,5 @@
 
 output "zone_id" { value = aws_route53_zone.primary.zone_id }
 output "route53_log_group_arn" { value = aws_cloudwatch_log_group.route53.arn }
-output "route53_kms_key_arn" { value = aws_kms_key.route53.arn }
+# output "route53_kms_key_arn" { value = aws_kms_key.route53.arn }
+output "route53_dnssec_kms_key_arn" {value = aws_kms_key.route53_dnssec.arn }
