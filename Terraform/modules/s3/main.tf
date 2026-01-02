1334fd143e908622a1158089fdcfa250d6b8b8da
ASADHASSAN <cybersecjunior00@gmail.com>
2026-01-02 13:40:41 +0000

    fixing Waf-Cloudfront

--- Terraform/modules/s3/main.tf
+++ Terraform/modules/s3/main.tf
@@ -25,45 +25,7 @@ resource "aws_kms_key" "s3" {
   })
 }
 
-# resource "aws_s3_bucket" "logs" {
-#    # checkov:skip=CKV_AWS_145:Encryption enforced via aws_s3_bucket_server_side_encryption_configuration
-#    # checkov:skip=CKV2_AWS_62:Event notifications enabled via aws_s3_bucket_notification
-#    # checkov:skip=CKV_AWS_18:Access logging configured via aws_s3_bucket_logging
-#    # checkov:skip=CKV2_AWS_61:Lifecycle rules defined via 
-#    # checkov:skip=CKV2_AWS_6:Public access block enforced via aws_s3_bucket_public_access_block
-#    # checkov:skip=CKV_AWS_21:Versioning enabled via aws_s3_bucket_versioning
-#    # checkov:skip=CKV_AWS_144:Replication not required for this environment
-#   bucket = "asad-portfolio-logs-bucket"
-#   tags   = var.tags
-# }
 
-resource "aws_s3_bucket_ownership_controls" "logs" {
-  # checkov:skip=CKV2_AWS_65:"Ensure access control lists for S3 buckets are disabled"
-  bucket = aws_s3_bucket.logs.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
-}
-
-resource "aws_s3_bucket_acl" "logs" {
-  depends_on = [aws_s3_bucket_ownership_controls.logs]
-
-  bucket = aws_s3_bucket.logs.id
-  acl    = "log-delivery-write"
-}
-
-
-resource "aws_s3_bucket_public_access_block" "logs" {
-  # checkov:skip=CKV_AWS_145:Encryption enforced via aws_s3_bucket_server_side_encryption_configuration
-  # checkov:skip=CKV_AWS_53:"Ensure S3 bucket has block public ACLS enabled"
-  # checkov:skip=CKV_AWS_55:"Ensure S3 bucket has ignore public ACLs enabled"
-  bucket                  = aws_s3_bucket.logs.id
-  block_public_acls       = false
-  block_public_policy     = true
-  ignore_public_acls      = false
-  restrict_public_buckets = true
-}
 resource "aws_s3_bucket" "buckets" {
   
  # checkov:skip=CKV_AWS_145:Encryption enforced via aws_s3_bucket_server_side_encryption_configuration
@@ -79,6 +41,34 @@ resource "aws_s3_bucket" "buckets" {
   tags   = var.tags
 
   depends_on = [ aws_kms_key.s3 ]
+}
+
+resource "aws_s3_bucket_ownership_controls" "logs" {
+  # checkov:skip=CKV2_AWS_65:"Ensure access control lists for S3 buckets are disabled"
+  bucket = aws_s3_bucket.buckets["asad-portfolio-logs-bucket"].id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "logs" {
+  depends_on = [aws_s3_bucket_ownership_controls.logs]
+
+  bucket = aws_s3_bucket.buckets["asad-portfolio-logs-bucket"].id
+  acl    = "log-delivery-write"
+}
+
+
+resource "aws_s3_bucket_public_access_block" "logs" {
+  # checkov:skip=CKV_AWS_145:Encryption enforced via aws_s3_bucket_server_side_encryption_configuration
+  # checkov:skip=CKV_AWS_53:"Ensure S3 bucket has block public ACLS enabled"
+  # checkov:skip=CKV_AWS_55:"Ensure S3 bucket has ignore public ACLs enabled"
+  bucket                  = aws_s3_bucket.buckets["asad-portfolio-logs-bucket"].id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "buckets" {
